e905d73cbd0370dd1d41b31116cfd737309dd905
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2025-11-05 15:48:14 -0500

    extract scheduling logic to greedy lib crate

--- crates/greedy-external/src/scheduler_thread.rs
+++ crates/greedy-external/src/scheduler_thread.rs
@@ -2,15 +2,13 @@ use std::path::PathBuf;
 use std::thread::JoinHandle;
 use std::time::Duration;
 
-use agave_scheduler_bindings::ProgressMessage;
-use agave_scheduling_utils::handshake::client::ClientSession;
 use agave_scheduling_utils::handshake::{ClientLogon, client as handshake_client};
+use greedy::GreedyScheduler;
 use toolbox::shutdown::Shutdown;
 
 pub(crate) struct SchedulerThread {
     shutdown: Shutdown,
-    session: ClientSession,
-    progress: ProgressMessage,
+    scheduler: GreedyScheduler,
 }
 
 impl SchedulerThread {
@@ -34,35 +32,14 @@ impl SchedulerThread {
                 )
                 .unwrap();
 
-                SchedulerThread {
-                    shutdown,
-                    session,
-                    progress: ProgressMessage {
-                        leader_state: 0,
-                        current_slot: 0,
-                        next_leader_slot: u64::MAX,
-                        leader_range_end: u64::MAX,
-                        remaining_cost_units: 0,
-                        current_slot_progress: 0,
-                    },
-                }
-                .run();
+                SchedulerThread { shutdown, scheduler: GreedyScheduler::new(session) }.run();
             })
             .unwrap()
     }
 
     fn run(mut self) {
         while !self.shutdown.is_shutdown() {
-            // Drain the progress tracker so we know which slot we're on.
-            self.drain_progress();
+            self.scheduler.poll();
         }
-    }
-
-    fn drain_progress(&mut self) {
-        self.session.progress_tracker.sync();
-        while let Some(msg) = self.session.progress_tracker.try_read() {
-            self.progress = *msg;
-        }
-        self.session.progress_tracker.finalize();
     }
 }
--- crates/greedy/src/lib.rs
+++ crates/greedy/src/lib.rs
@@ -1,1 +1,59 @@
+use agave_scheduler_bindings::{ProgressMessage, TpuToPackMessage};
+use agave_scheduling_utils::handshake::client::{ClientSession, ClientWorkerSession};
+use rts_alloc::Allocator;
 
+pub struct GreedyScheduler {
+    allocator: Allocator,
+    tpu_to_pack: shaq::Consumer<TpuToPackMessage>,
+    progress_tracker: shaq::Consumer<ProgressMessage>,
+    workers: Vec<ClientWorkerSession>,
+    progress: ProgressMessage,
+}
+
+impl GreedyScheduler {
+    pub fn new(
+        ClientSession { mut allocators, tpu_to_pack, progress_tracker, workers }: ClientSession,
+    ) -> Self {
+        assert_eq!(allocators.len(), 1, "invalid number of allocators");
+
+        Self {
+            allocator: allocators.remove(0),
+            tpu_to_pack,
+            progress_tracker,
+            workers,
+            progress: ProgressMessage {
+                leader_state: 0,
+                current_slot: 0,
+                next_leader_slot: u64::MAX,
+                leader_range_end: u64::MAX,
+                remaining_cost_units: 0,
+                current_slot_progress: 0,
+            },
+        }
+    }
+
+    pub fn poll(&mut self) {
+        // Drain the progress tracker so we know which slot we're on.
+        self.drain_progress();
+
+        // Drain responses from workers.
+        self.drain_worker_responses();
+    }
+
+    fn drain_progress(&mut self) {
+        self.progress_tracker.sync();
+        while let Some(msg) = self.progress_tracker.try_read() {
+            self.progress = *msg;
+        }
+        self.progress_tracker.finalize();
+    }
+
+    fn drain_worker_responses(&mut self) {
+        for worker in &mut self.workers {
+            worker.worker_to_pack.sync();
+            while let Some(msg) = worker.worker_to_pack.try_read() {
+                todo!()
+            }
+        }
+    }
+}
