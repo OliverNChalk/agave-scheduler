68b3d56812bf2e83460c3ae0c35c8b31cf325c7b
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2025-11-12 15:14:39 -0500

    j

--- crates/greedy/src/lib.rs
+++ crates/greedy/src/lib.rs
@@ -171,7 +171,7 @@ impl GreedyScheduler {
                         }
 
                         // Free both containers.
-                        batch.free();
+                        batch.free(); // OLI: This will double free the transactions in the batch.
                         responses.free();
                     }
                     worker_message_types::EXECUTION_RESPONSE => {
