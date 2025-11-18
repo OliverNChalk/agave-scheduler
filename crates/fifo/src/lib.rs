82cad2be130ae619835efb7a2799a7ac2486f34d
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2025-11-18 13:29:34 -0500

    extract scheduler core to own crate as `bridge`

--- crates/fifo/src/lib.rs
+++ crates/fifo/src/lib.rs
@@ -8,6 +8,7 @@ use agave_scheduler_bindings::{
     IS_LEADER, MAX_TRANSACTIONS_PER_MESSAGE, ProgressMessage, pack_message_flags,
 };
 use agave_scheduling_utils::transaction_ptr::TransactionPtr;
+use bridge::{Bridge, TpuDecision, TransactionId, WorkerId};
 
 // TODO:
 //
@@ -23,18 +24,21 @@ use agave_scheduling_utils::transaction_ptr::TransactionPtr;
 const CHECK_WORKER: WorkerId = WorkerId;
 const EXECUTE_WORKER: WorkerId = WorkerId;
 
-pub struct FifoScheduler {
-    core: SchedulerCore,
+pub struct FifoScheduler<B> {
+    bridge: B,
     check_queue: VecDeque<TransactionId>,
     execute_queue: VecDeque<TransactionId>,
     batch: Vec<TransactionId>,
 }
 
-impl FifoScheduler {
+impl<B> FifoScheduler<B>
+where
+    B: Bridge,
+{
     #[must_use]
-    pub fn new(core: SchedulerCore) -> Self {
+    pub fn new(bridge: B) -> Self {
         Self {
-            core,
+            bridge,
             check_queue: VecDeque::default(),
             execute_queue: VecDeque::default(),
             batch: Vec::with_capacity(MAX_TRANSACTIONS_PER_MESSAGE),
@@ -43,11 +47,11 @@ impl FifoScheduler {
 
     pub fn poll(&mut self) {
         // Drain the progress tracker so we know which slot we're on.
-        self.core.drain_progress();
-        let is_leader = self.core.progress().leader_state == IS_LEADER;
+        self.bridge.drain_progress();
+        let is_leader = self.bridge.progress().leader_state == IS_LEADER;
 
         // Drain check responses.
-        while self.core.pop_check(|id, _, rep| {
+        while self.bridge.pop_check(|id, _, rep| {
             // TODO: Dedupe with greedy & make this friendlier.
             let parsing_failed =
                 rep.parsing_and_sanitization_flags == parsing_and_sanitization_flags::FAILED;
@@ -66,14 +70,14 @@ impl FifoScheduler {
         }) {}
 
         // Drain execute responses.
-        while self.core.pop_execute(|_, _, _| TpuDecision::Drop) {}
+        while self.bridge.pop_execute(|_, _, _| TpuDecision::Drop) {}
 
         // Ingest a bounded amount of new transactions.
         let max_count = match is_leader {
             true => 128,
             false => 1024,
         };
-        self.core.drain_tpu(
+        self.bridge.drain_tpu(
             |(id, _)| {
                 self.check_queue.push_back(id);
 
@@ -82,18 +86,19 @@ impl FifoScheduler {
             max_count,
         );
 
+        // Schedule checks & execution (if we're the leader).
         self.schedule();
     }
 
     fn schedule(&mut self) {
         // Schedule additional checks.
-        while self.core.worker(CHECK_WORKER).rem() > 0 {
+        while self.bridge.worker(CHECK_WORKER).rem() > 0 {
             self.batch.clear();
             self.batch.extend(
                 std::iter::from_fn(|| self.check_queue.pop_front())
                     .take(MAX_TRANSACTIONS_PER_MESSAGE),
             );
-            self.core.schedule_check(
+            self.bridge.schedule_check(
                 CHECK_WORKER,
                 &self.batch,
                 u64::MAX,
@@ -105,94 +110,20 @@ impl FifoScheduler {
         }
 
         // If we are the leader, schedule executes.
-        if self.core.progress().leader_state == IS_LEADER {
+        if self.bridge.progress().leader_state == IS_LEADER
+            && self.bridge.worker(EXECUTE_WORKER).len() == 0
+        {
             self.batch.clear();
             self.batch.extend(
                 std::iter::from_fn(|| self.execute_queue.pop_front())
                     .take(MAX_TRANSACTIONS_PER_MESSAGE),
             );
-            self.core.schedule_execute(
+            self.bridge.schedule_execute(
                 EXECUTE_WORKER,
                 &self.batch,
-                self.core.progress().current_slot + 1,
+                self.bridge.progress().current_slot + 1,
                 pack_message_flags::EXECUTE,
             );
         }
     }
-}
-
-struct SchedulerCore;
-
-impl SchedulerCore {
-    fn progress(&self) -> &ProgressMessage {
-        todo!()
-    }
-
-    fn worker(&self, id: WorkerId) -> &Worker {
-        todo!()
-    }
-
-    fn drain_progress(&mut self) {
-        todo!()
-    }
-
-    fn drain_tpu(
-        &mut self,
-        cb: impl FnMut((TransactionId, TransactionPtr)) -> TpuDecision,
-        max_count: usize,
-    ) {
-        todo!()
-    }
-
-    fn pop_check(
-        &mut self,
-        cb: impl FnMut(TransactionId, &TransactionPtr, &CheckResponse) -> TpuDecision,
-    ) -> bool {
-        todo!()
-    }
-
-    fn pop_execute(
-        &mut self,
-        cb: impl FnMut(TransactionId, &TransactionPtr, &ExecutionResponse) -> TpuDecision,
-    ) -> bool {
-        todo!()
-    }
-
-    fn schedule_check(
-        &mut self,
-        worker: WorkerId,
-        batch: &[TransactionId],
-        max_working_slot: u64,
-        flags: u16,
-    ) {
-        todo!()
-    }
-
-    fn schedule_execute(
-        &mut self,
-        worker: WorkerId,
-        batch: &[TransactionId],
-        max_working_slot: u64,
-        flags: u16,
-    ) {
-        todo!()
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-struct WorkerId;
-
-struct Worker;
-
-impl Worker {
-    fn rem(&self) -> usize {
-        todo!()
-    }
-}
-
-struct TransactionId;
-
-enum TpuDecision {
-    Keep,
-    Drop,
 }
