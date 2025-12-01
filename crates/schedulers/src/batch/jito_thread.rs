14931332a29a67b7ea71c6479fe654324c2bc8bc
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2025-12-01 09:54:33 -0500

    working through bundle/packet client

--- crates/schedulers/src/batch/jito_thread.rs
+++ crates/schedulers/src/batch/jito_thread.rs
@@ -1,21 +1,161 @@
+use std::convert::Infallible;
+use std::sync::{Arc, Mutex};
 use std::thread::JoinHandle;
+use std::time::Duration;
 
-pub(crate) struct JitoThread {}
+use eyre::eyre;
+use jito_protos::auth::auth_service_client::AuthServiceClient;
+use jito_protos::auth::{GenerateAuthChallengeRequest, GenerateAuthTokensRequest, Role, Token};
+use jito_protos::block_engine::block_engine_validator_client::BlockEngineValidatorClient;
+use jito_protos::block_engine::{
+    SubscribeBundlesRequest, SubscribeBundlesResponse, SubscribePacketsRequest,
+    SubscribePacketsResponse,
+};
+use solana_keypair::{Keypair, Signer};
+use tonic::service::Interceptor;
+use tonic::transport::{Channel, ClientTlsConfig, Endpoint};
+use tonic::{Request, Status};
+
+pub(crate) struct JitoConfig {
+    url: String,
+}
+
+pub(crate) struct JitoThread {
+    endpoint: Endpoint,
+    keypair: Keypair,
+}
 
 impl JitoThread {
-    pub(crate) fn spawn(bundle_tx: crossbeam_channel::Sender<()>) -> JoinHandle<()> {
+    pub(crate) fn spawn(
+        bundle_tx: crossbeam_channel::Sender<()>,
+        config: JitoConfig,
+        keypair: Keypair,
+    ) -> JoinHandle<()> {
         let rt = tokio::runtime::Builder::new_current_thread()
             .enable_all()
             .build()
             .unwrap();
+        // Setup the block engine endpoint.
+        let enable_tls = config.url.starts_with("https");
+        let mut endpoint = Endpoint::from_shared(config.url)
+            .unwrap()
+            .tcp_keepalive(Some(Duration::from_secs(60)));
+        if enable_tls {
+            endpoint = endpoint.tls_config(ClientTlsConfig::new()).unwrap();
+        }
 
         std::thread::Builder::new()
             .name("Jito".to_string())
-            .spawn(move || rt.block_on(JitoThread {}.run()))
+            .spawn(move || rt.block_on(JitoThread { endpoint, keypair }.run()))
             .unwrap()
     }
 
     async fn run(self) {
+        loop {
+            let Err(err) = self.run_until_err().await;
+            println!("{err:?}");
+        }
+    }
+
+    async fn run_until_err(&self) -> eyre::Result<Infallible> {
+        // Connect to the auth service.
+        let auth = self.endpoint.connect().await?;
+        let mut auth = AuthServiceClient::new(auth);
+
+        // Complete the block engine auth challenge.
+        let pubkey = self.keypair.pubkey();
+        let challenge_response = auth
+            .generate_auth_challenge(GenerateAuthChallengeRequest {
+                role: Role::Validator as i32,
+                pubkey: pubkey.as_array().to_vec(),
+            })
+            .await?;
+        let formatted_challenge = format!("{pubkey}-{}", challenge_response.into_inner().challenge);
+        let signed_challenge = self
+            .keypair
+            .sign_message(formatted_challenge.as_bytes())
+            .as_array()
+            .to_vec();
+
+        // Generate auth tokens using the signed challenge.
+        let auth_tokens = auth
+            .generate_auth_tokens(GenerateAuthTokensRequest {
+                challenge: formatted_challenge,
+                client_pubkey: pubkey.as_array().to_vec(),
+                signed_challenge,
+            })
+            .await?
+            .into_inner();
+
+        // Extract & validate tokens.
+        let access = auth_tokens
+            .access_token
+            .ok_or_else(|| eyre!("Missing access token"))?;
+        eyre::ensure!(access.expires_at_utc.is_some(), "Missing access expiry");
+        let refresh = auth_tokens
+            .refresh_token
+            .ok_or_else(|| eyre!("Missing refresh token"))?;
+        eyre::ensure!(refresh.expires_at_utc.is_some(), "Missing refresh expiry");
+
+        // Connect to the block engine service.
+        let access = Arc::new(Mutex::new(access));
+        let block_engine = self.endpoint.connect().await?;
+        let mut block_engine = BlockEngineValidatorClient::with_interceptor(
+            block_engine,
+            AuthInterceptor { access: access.clone() },
+        );
+
+        // Start the bundle & packet streams.
+        let mut bundles = block_engine
+            .subscribe_bundles(SubscribeBundlesRequest {})
+            .await?
+            .into_inner();
+        let mut packets = block_engine
+            .subscribe_packets(SubscribePacketsRequest {})
+            .await?
+            .into_inner();
+
+        // Consume bundles & packets until error.
+        loop {
+            tokio::select! {
+                biased;
+
+                res = bundles.message() => {
+                    let bundles = res?.ok_or_else(|| eyre!("bundle stream closed"))?;
+
+                    self.on_bundles(bundles);
+                }
+                res = packets.message() => {
+                    let packets = res?.ok_or_else(|| eyre!("bundle stream closed"))?;
+
+                    self.on_packets(packets);
+                },
+            }
+        }
+    }
+
+    fn on_bundles(&self, bundles: SubscribeBundlesResponse) {
         todo!()
+    }
+
+    fn on_packets(&self, packets: SubscribePacketsResponse) {
+        todo!()
+    }
+}
+
+struct AuthInterceptor {
+    access: Arc<Mutex<Token>>,
+}
+
+impl Interceptor for AuthInterceptor {
+    fn call(&mut self, mut request: Request<()>) -> Result<Request<()>, Status> {
+        request.metadata_mut().insert(
+            "authorization",
+            format!("Bearer {}", self.access.lock().unwrap().value)
+                .parse()
+                .map_err(|_| Status::invalid_argument("Failed to parse authorization token"))?,
+        );
+
+        Ok(request)
     }
 }
